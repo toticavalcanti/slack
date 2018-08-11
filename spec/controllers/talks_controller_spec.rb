3a5a407f16d73d639b90f0f6ee2d8f16bcbd1215
toticavalcanti <toticavalcanti@hotmail.com>
2018-08-11 17:16:24 -0300

    create and set /spec/controllers/team_users_controller_spec.rb

--- spec/controllers/talks_controller_spec.rb
+++ spec/controllers/talks_controller_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe TalksController, type: :controller do
     request.env["HTTP_ACCEPT"] = 'application/json'
 
     @request.env["devise.mapping"] = Devise.mappings[:user]
-    @current_user = FactoryGirl.create(:user)
+    @current_user = FactoryBot.create(:user)
     sign_in @current_user
   end
 
